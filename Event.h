2629e16bb9dde2d4b68f49c9c84dde96832f25a7
pilafnoodle <amberlin618@gmail.com>
2023-11-10 11:41:18 -0800

    added several Event constructors and print function

--- Event.h
+++ Event.h
@@ -1,8 +1,18 @@
 #ifndef __EVENT_H__
 #define __EVENT_H__
 
+#include <string>
+using namespace std;
+
 class Event{
-    public:
+    public:        
+        Event(string name, string category,int priority,string description);
+        Event(string name, string category,int priority);
+        Event(string name, string category);
+        Event(string name);
+        Event();
+        void printTask();
+    private: 
         string name;
         string category;
         int priority;
