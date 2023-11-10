74dfbf92a816080e928744cc2758affbc5a2385f
mouremr <iammarcel08@gmail.com>
2023-11-10 11:27:27 -0800

    start implementation for Schedule class

--- Schedule.cpp
+++ Schedule.cpp
@@ -1,2 +1,11 @@
 #include "Schedule.h"
+#include "Event.h"
+#include <vector>
 
+class Schedule{
+    private vector<event> hours[24];
+}
+
+vector<Event> Schedule::makeSchedule{
+    
+}
--- Schedule.h
+++ Schedule.h
@@ -1,8 +1,11 @@
 #ifndef __SCHEDULE_H__
 #define __SCHEDULE_H__
+#include <vector>
+#include "Event.h"
 
 class Schedule{
-    
+    private:
+        std::vector<Event> hours[24];
 };
 
 
