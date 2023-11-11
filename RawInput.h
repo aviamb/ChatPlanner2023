aa25906d4b11a97c15afc9cb091c88c0adaa4ea0
Avia Bequette <abequ001@xe-01.cs.ucr.edu>
2023-11-11 13:45:23 -0800

    Created function declarations for RawInput class

--- RawInput.h
+++ RawInput.h
@@ -1,8 +1,19 @@
 #ifndef __RAWINPUT_H__
 #define __RAWINPUT_H__
 
-class RawInput{
+#include <vector>
 
+class RawInput{
+    private:
+    vector<Event> rawTaskList;
+    vector<bool> prefList;
+    
+    public: 
+    RawInput();
+    void askPreferences(); 
+    void askTasks(); //terminated by q
 };
 
 #endif
+
+//preferences order:
