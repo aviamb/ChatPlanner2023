22a8ea8d0fb8108315eea9fef8d699f4b0e23c1f
pilafnoodle <amberlin618@gmail.com>
2023-11-14 15:30:07 -0800

    main psuedo code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,27 @@
-//put all driver code here
+#include "Event.h"
+#include "Schedule.h"
+#include "RawInput.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    //ask tasks
+
+    //ask preferences
+
+    //while save==false
+    //display schedule
+    //print out menu
+    //if(add task)
+    //add task to next available slot in schedule
+    //if(checkoff task)
+    //set that tasks timeslot to none
+    //if(save)
+    //save to txt and end program
+    
+
+    return 1;
+};
