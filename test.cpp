eb89c8fc4ba277837c800982edb1c6b95b71877e
Sangha0822 <sangha082214@gmail.com>
2023-11-22 13:35:36 -0800

    googletest editing

--- test.cpp
+++ test.cpp
@@ -1,10 +1,10 @@
 #include "RawInput.cpp"
 #include "gtest/gtest.h"
-
+#include <fstream>
 
 TEST(AskPreferenceTests, testingYesNoYes) {//should pass, but testing failing values
     ifstream ifs;
-    ifs.open()
+    ifs.open("googleTest.txt", )
     RawInput *aRawInput = new RawInput();
     int area = aRawInput->askPreferences();
     EXPECT_FALSE (area == 13);
